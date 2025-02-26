ef3492949323c78ce75d428664bf56c6c06ec302
Lakshmi1799 <lakshmisravanthi017@gmail.com>
2025-02-26 15:22:36 +0530

    Update Cloudinfra.tf

--- Cloudinfra.tf
+++ Cloudinfra.tf
@@ -4,6 +4,6 @@ region = "ap-south-1"
 #create s3 bucket
 resource "aws_s3_bucket" "ccit_s3" {
 bucket = "ramsrava-bucket"
-tags = {Name = "ramsrava-bucket" 
+tags = {Name = "ramsrava17-bucket" 
 }
 }
